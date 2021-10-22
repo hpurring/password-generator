9d04afea2ddb00131f1154a538e6a1b9625838a0
hpurring <hilarypurrington@gmail.com>
2021-10-21 22:56:03 -0400

    updated all prompts and validations

--- Develop/script.js
+++ Develop/script.js
@@ -1,96 +1,5 @@
 // Assignment code here
 
-// Array of special characters to be included in password
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
-// Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
-
-
- 
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -101,23 +10,37 @@ function generatePassword() {
     alert(`Your new password will be ${totalLength} characters long.`);
   } else {
     alert('Your password must be 8-128 characters!');
+    return("Try again. This time, follow directions.")
   }
   
-  // var options = {
-  //   hasNumber: window.confirm('Would you like to include numbers?'),
-  //   hasUppercase: window.confirm('Would you like to include uppercase letters?'),
-  //   hasLowercase: window.confirm('Would you like to include lowercase letters?'),
-  //   hasSymbol: window.confirm('Would you like to include special characters?'),
-  // };
-
-  // if (options.hasLowercase === false && options.hasUppercase === false && options.hasSymbol === false) {
-    
-  // }
-
   let upper = confirm("Would you like to include uppercase letters?");
+    if (upper) {
+      alert("Excellent choice. We'll include uppercase letters.");
+    } else {
+      alert("Ok, no uppercase letters.")
+    }
   let lower = confirm("Would you like to include lowercase letters?");
+    if (lower) {
+      alert("You have exquisite taste. We'll bring out the lowercase letters.");
+    } else {
+      alert("Gotcha, no lowercase letters.")
+    }
   let symbol = confirm("Would you like to include special characters?")
+    if (symbol) {
+      alert("Of course. We'll select the most special characters for you.");
+    } else {
+      alert("Copy that. Absolutely no special characters.")
+    }
   let number = confirm("Would you like to include numbers?");
+    if (number) {
+      alert("Our numbers are fantastic. You won't regret this.");
+    } else {
+      alert("We hear you. NO numbers.")
+    }
+
+  if (!upper && !lower && !symbol && !number) {
+    alert("You must use at least one type of character! Try again.")
+  }
 
   let allChars = "";
   let password = "";
@@ -142,7 +65,6 @@ function generatePassword() {
    
   // Generate password that matches the selected criteria
   // create a for loop to iterate over the chosen characters
-
  
   for (var i = 0, n = totalLength; i < totalLength; i++) {
     //picks a character within charSet at index of random number
@@ -165,4 +87,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-writePassword();
